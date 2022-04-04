c1827c0d530d833b9e3fb28a1a5b0a7c79bd9c66
Avisheikbarua1158 <avisheik.barua@northsouth.edu>
2022-04-04 23:05:04 +0600

    Home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Slogan from '../Slogan/Slogan';
 
 const Home = () => {
     return (
-        <div>
-            This is Home
+        <div className='grid  md:grid-cols-2 gap-18 '  >
+            <Slogan></Slogan>
+            <img className='mt-20' src="https://www.re-thinkingthefuture.com/wp-content/uploads/2021/04/A3848-How-to-enhance-Soft-Furnishing-Aesthetics-in-your-house.jpg" alt="" />
         </div>
+
     );
 };
 
---
+++ src/components/Slogan/Slogan.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Slogan = () => {
+    return (
+        <div className='mt-20'>
+            <h2 className='text-6xl text-rose-600 font-bold '>
+                Your perfect room
+            </h2>
+            <h2 className='text-6xl text-sky-600 font-bold'>
+                Decorate your Life with Arts
+            </h2>
+            <p className='font-style: italic'>
+                The significance of furniture in our day-by-day life has regularly been a dismissed subject. Individuals know they require diverse things to have an agreeable existence, yet regularly the expense of the furniture is either dismissed, or legitimate plans are attracted out to meet the costs required to purchase the furniture.
+            </p>
+        </div>
+    );
+};
+
+export default Slogan;
